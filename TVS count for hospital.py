84dd6fd1db4393a07243fd518a908318f246ac78
M.Giridhara Rao <www.giridhararao@gmail.com>
2020-12-09 22:28:55 +0530

    Create TVS count for hospital.py
    Input: N = 20, R1 = 1500, R2 = 1000, target = 7000000
    Output: 14
    Explanation:
    Using the formula:
    The number of patients on 1st Jan will be 39, on 2nd Jan will be 38 and so on.
    Considering there are only twenty rooms and rates of both type of rooms are 1500 and 1000 respectively.
    Therefore, 14 TV sets are needed to get the revenue of 7119500 with 13 TV sets.
    The total revenue will be less than 7000000.
    
    Input: N = 10, R1 = 1000, R2 = 1500, target = 10000000
    Output: 10
    Explanation:
    In the above example, the target will not be achieved, even by equipping all the rooms with TV.
    Hence, the answer is 10 i.e., total number of rooms in the hospital.

---
+++ TVS count for hospital.py
@@ -0,0 +1,11 @@
+def getpersoncount(D,M):
+    return(((6-M)**2)+abs(D-15))
+
+def getval(N,R1,R2,target):
+    tvcount=0
+    peoplecount=0
+    currenttarget=0
+    Months=[31,28,31,30,31,30,31,31,30,31,30,31]
+    for i in range()
+
+N,R1,R2,target=20,1500,1000,7000000
