bb54768626c25b4f523aae41e22b16b7bbd65f45
M.Giridhara Rao <www.giridhararao@gmail.com>
2020-12-12 22:25:56 +0530

    Update stair case climbing with 1 or 2 steps.py
    in the place of recursion used list to append the data.

--- stair case climbing with 1 or 2 steps.py
+++ stair case climbing with 1 or 2 steps.py
@@ -1,9 +1,11 @@
 def check(a):
-        if(a<=1):
+        b=[0 for x in range(a+1)]
+        if(a==1 or a==0):
                 return(1)
-        elif(a==2):
-                return(2)
-        else:
-                return(check(a-1)+check(a-2))
+        b[0]=1
+        b[1]=1
+        for i in range(2,a+1):
+                b[i]=b[i-1]+b[i-2]
+        return(b[a])
 a=int(input())
 print(check(a))
