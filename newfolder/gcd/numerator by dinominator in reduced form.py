890d94517e44c4b2923f24afe70e7f2885c9a999
M.Giridhara Rao <www.giridhararao@gmail.com>
2021-01-05 21:21:11 +0530

    Create numerator by dinominator in reduced form.py
    Input : N = 3
            num[] = { 1, 2, 5 }
            den[] = { 2, 1, 6 }
    Output : 5/6
    Product of 1/2 * 2/1 * 5/6 is 10/12.
    Reduced form of 10/12 is 5/6.
    
    Input : N = 4
            num[] = { 1, 2, 5, 9 }
            den[] = { 2, 1, 6, 27 }
    Output : 5/18

---
+++ newfolder/gcd/numerator by dinominator in reduced form.py
@@ -0,0 +1,17 @@
+def gcd(a,b):
+        while(b):
+                a,b=b,b%a
+        return(a)
+
+a=[int(x) for x in input().split()]
+b=[int(x) for x in input().split()]
+num1=1
+for i in a:
+        num1*=i
+num2=1
+for i in b:
+        num2*=i
+c=gcd(num1,num2)
+num1=int(num1/c)
+num2=int(num2/c)
+print(str(num1)+"/"+str(num2))
