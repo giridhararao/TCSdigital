299ccb38fc03aa7a09a0d9d7af54361d3cd7c3f9
M.Giridhara Rao <www.giridhararao@gmail.com>
2021-06-22 02:46:47 +0100

    Create check rotation string.py
    Input:
    abcdef
    defabc
    Output:
    string1 and string2 are rotations of each other

---
+++ wiproturbo/check rotation string.py
@@ -0,0 +1,7 @@
+a=input()
+b=input()
+tmp=str(a)+str(a)
+if(b in tmp):
+    print("string1 and string2 are rotations of each other")
+else:
+    print("string1 and string2 are not rotations of each other")
